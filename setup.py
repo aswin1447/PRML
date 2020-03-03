6ff4a4a0ad653399118ba1f1d4d0445436aa5cdb
Kyle Cranmer <kyle.cranmer@nyu.edu>
2020-03-03 14:17:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description="Collection of PRML algorithms",
     author="ctgk",
     python_requires=">=3.6",
-    install_requires=["numpy", "scipy"],
+    install_requires=["numpy", "scipy", "matplotlib", "scikit-learn"],
     packages=find_packages(exclude=["test", "test.*"]),
     test_suite="test"
 )
